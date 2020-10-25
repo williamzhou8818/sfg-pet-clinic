8bb6a3ef2d5613ab5d336d22c2bd1c1b07c2e9fa
Li Zhou <lizhou@MacBook-Pro.local>
2020-10-25 11:48:49 +1100

    implement vet controller

--- src/main/java/guru/springframework/sfgpetclinic/services/OwerService.java
+++ src/main/java/guru/springframework/sfgpetclinic/services/OwerService.java
@@ -1,18 +1,10 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
-
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public interface OwerService {
+public interface OwerService extends CrudService<Owner, Long> {
 	
 	  Owner findByLastName(String lastName);
-	
-	  Owner findById(Long id);
-	  
-	  Owner save(Owner owner);
-	  
-	  Set<Owner> findAll(); 	  
 	  
 
 }
